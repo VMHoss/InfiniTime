188475910d5bbb234dbaec54a407a43e768ed3e6
4DS <68349351+VMHoss@users.noreply.github.com>
2025-03-06 16:32:24 +0000

    Update WatchFaceSJ.cpp

--- src/displayapp/screens/WatchFaceSJ.cpp
+++ src/displayapp/screens/WatchFaceSJ.cpp
@@ -71,7 +71,7 @@ WatchFaceSJ::WatchFaceSJ(Controllers::DateTime& dateTimeController,
   lv_obj_set_style_local_text_font(label_time, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_extrabold_compressed);
   lv_obj_set_style_local_text_color(label_time, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, lv_color_hex(0x28ACF0));
 
-  lv_obj_align(label_time, lv_scr_act(), LV_ALIGN_IN_RIGHT_MID, 0, 20);
+  lv_obj_align(label_time, lv_scr_act(), LV_ALIGN_IN_RIGHT_MID, 0, 80);
 
   label_time_ampm = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_text_static(label_time_ampm, "");
