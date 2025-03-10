fab9663903833b6b7a1f1323d39bce7613295f9f
4DS <68349351+VMHoss@users.noreply.github.com>
2025-03-10 16:54:08 +0000

    Update WatchFaceSJ.cpp

--- src/displayapp/screens/WatchFaceSJ.cpp
+++ src/displayapp/screens/WatchFaceSJ.cpp
@@ -84,7 +84,7 @@ WatchFaceSJ::WatchFaceSJ(Controllers::DateTime& dateTimeController,
   stepIcon = lv_label_create(lv_scr_act(), nullptr);
   lv_obj_set_style_local_text_color(stepIcon, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, lv_color_hex(0x28ACF0));
   lv_label_set_text_static(stepIcon, "SP");
-  lv_obj_align(stepIcon, stepValue, LV_ALIGN_IN_BOTTOM_LEFT, -5, 0);
+  lv_obj_align(stepIcon, stepValue, LV_ALIGN_IN_LEFT_MID, -5, 0);
 
   taskRefresh = lv_task_create(RefreshTaskCallback, LV_DISP_DEF_REFR_PERIOD, LV_TASK_PRIO_MID, this);
   Refresh();
